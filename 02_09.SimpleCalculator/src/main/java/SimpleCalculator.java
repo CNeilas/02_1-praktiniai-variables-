e827a85f6d970458595b91289031442222ffa10a
nsunklodas <farmeris108@gmail.com>
2024-11-05 21:41:38 +0200

    agaiiiiiiiin

---
+++ 02_09.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int number1 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int number2 = Integer.parseInt(scanner.nextLine());
+
+        int sum = number1 + number2;
+        int difference = number1 - number2;
+        int product = number1 * number2;
+        double quotient = (double) number1 / number2;
+        System.out.println(number1 + " + " + number2 + " = " + sum);
+        System.out.println(number1 + " - " + number2 + " = " + difference);
+        System.out.println(number1 + " * " + number2 + " = " + product);
+        System.out.println(number1 + " / " + number2 + " = " + quotient);
+
+    }
+}
